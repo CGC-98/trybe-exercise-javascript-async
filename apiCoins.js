1ced33c802a21bdb945630af55da763ef8985266
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-10-09 17:07:30 -0300

    project completion

--- apiCoins.js
+++ apiCoins.js
@@ -1,23 +1,31 @@
 // const fetch = require('node-fetch');
 
+const assetsURL = 'https://api.coincap.io/v2/assets';
+const cur = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/brl.json';
+const postList = document.getElementById('coins-list');
+const convList = document.getElementById('convert-list');
+
+const createLi = (dataTen, listID, brl = 1) => {
+  dataTen.forEach(({ name, symbol, priceUsd }) => {
+    const newLi = document.createElement('li');
+    newLi.innerHTML = `${name} (${symbol}): ${(Number(priceUsd) * brl).toFixed(2)}`;
+    listID.appendChild(newLi);
+  });
+};
+
 const fetchCoins = async () => {
-  const assetsURL = 'https://api.coincap.io/v2/assets';
-  const postList = document.getElementById('coins-list');
-
   try {
-    const response = await fetch(assetsURL);
-    const data = await response.json();
-    const { data: dataKey } = data;
-    const dataKeyTen = dataKey.slice(0, 10);
-
-    dataKeyTen.forEach(({ name, symbol, priceUsd }) => {
-      const newLi = document.createElement('li');
-      newLi.innerHTML = `${name} (${symbol}): ${Number(priceUsd).toFixed(2)}`;
-      postList.appendChild(newLi);
-    });
-  } catch (err) {
-    console.log(err);
-  }
+    const respAssets = await fetch(assetsURL);
+    const dataAssets = await respAssets.json();
+    const { data } = dataAssets;
+    const dataTen = data.slice(0, 10);
+    createLi(dataTen, postList);
+    
+    const respConvert = await fetch(cur);
+    const dataConvert = await respConvert.json();
+    const { brl } = dataConvert;
+    createLi(dataTen, convList, brl);
+  } catch (err) { console.log(err); }
 };
 
 fetchCoins();
