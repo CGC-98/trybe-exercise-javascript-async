7b83d5c4c0ae1785acee1e6c3870c2c9dde94450
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-10-09 15:56:44 -0300

    up to 3

--- apiCoins.js
+++ apiCoins.js
@@ -1,4 +1,23 @@
+// const fetch = require('node-fetch');
+
 const fetchCoins = async () => {
+  const assetsURL = 'https://api.coincap.io/v2/assets';
+  const postList = document.getElementById('coins-list');
+
+  try {
+    const response = await fetch(assetsURL);
+    const data = await response.json();
+    const { data: dataKey } = data;
+    const dataKeyTen = dataKey.slice(0, 10);
+
+    dataKeyTen.forEach(({ name, symbol, priceUsd }) => {
+      const newLi = document.createElement('li');
+      newLi.innerHTML = `${name} (${symbol}): ${Number(priceUsd).toFixed(2)}`;
+      postList.appendChild(newLi);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 fetchCoins();
